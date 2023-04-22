e8c582ac4c1299eda89044618d0f0c2cba9c097a
drew-beamer <drbeamer@davidson.edu>
2023-04-22 16:15:24 -0400

    champs update

--- next/src/app/events/[slug]/page.tsx
+++ next/src/app/events/[slug]/page.tsx
@@ -5,6 +5,12 @@ import { getEvents } from "@/lib/mongo/events";
 import { Projection } from "@/lib/types/event";
 import Link from "next/link";
 
+
+const HOF_GRADIENT = "bg-gradient-to-br from-sky-100 via-sky-400  to-sky-100 text-white"
+const CHAIRMANS_GRADIENT = "bg-gradient-to-br from-yellow-300 via-yellow-400 to-yellow-500"
+const EI_GRADIENT = "bg-gradient-to-br from-gray-100 to-gray-300"
+const OTHER_GRADIENT = "bg-gradient-to-br from-yellow-600 to-red-600 text-stone-50"
+
 export async function generateStaticParams() {
     const allEvents = await getEvents({ fields: [] });
     return allEvents.map((event) => {
@@ -37,7 +43,7 @@ export async function generateMetadata({ params }: { params: { slug: string } })
     }
 }
 
-function ProjectionsTable({ data, impactWinners, eiWinners }: { data: Projection[], impactWinners: string[], eiWinners: string[] }) {
+function ProjectionsTable({ data, impactWinners, eiWinners, hofWinners, awardWinners }: { data: Projection[], impactWinners: string[], eiWinners: string[], hofWinners: string[], awardWinners: string[] }) {
     return <div className="relative overflow-x-auto mt-6 z-0">
         <table className="w-full text-sm text-left px-6 mt-6 overflow-x-scroll">
             <thead className="text-xs uppercase bg-gray-50">
@@ -48,10 +54,10 @@ function ProjectionsTable({ data, impactWinners, eiWinners }: { data: Projection
                     <th scope="col" className="px-6 py-3">Power</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody className="w-full">
                 {data?.map((team, index) => {
-                    return <tr key={team[0].key} className={`border-b rounded-lg ${impactWinners.includes(team[0].key) ? "bg-gradient-to-br from-yellow-300 via-yellow-400 to-yellow-500" : eiWinners.includes(team[0].key) ? "bg-gradient-to-br from-gray-100 to-gray-300" : ""}`}>
-                        <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap">{index + 1}</th>
+                    return <tr style={{backgroundAttachment: "fixed"}} key={team[0].key} className={`border-b w-full rounded-lg ${hofWinners.includes(team[0].key) ? HOF_GRADIENT : impactWinners.includes(team[0].key) ? CHAIRMANS_GRADIENT : eiWinners.includes(team[0].key) ? EI_GRADIENT : awardWinners.includes(team[0].key) ? OTHER_GRADIENT : ""}`}>
+                        <td className="px-6 py-4 font-medium whitespace-nowrap">{index + 1}</td>
                         <td className="px-6 py-4"><Link href={`/teams/${team[0].key?.substring(3)}`}>{team[0].key?.substring(3)}</Link></td>
                         <td className="px-6 py-4"><Link href={`/teams/${team[0].key?.substring(3)}`}>{team[0].name}</Link></td>
                         <td className="px-6 py-4">{Math.round(100 * team[1]) / 100}</td>
@@ -67,14 +73,22 @@ function ProjectionsTable({ data, impactWinners, eiWinners }: { data: Projection
 export default async function Page({ params }: { params: { slug: string } }) {
     const eventData = await getEvents({ fields: ["awards", "end_date", "projections", "ei_projections", "week"], eventCodes: [params.slug] }).then((arr) => arr[0]);
 
+    let hofWinners: string[] = []
     let impactWinners: string[] = []
     let eiWinners: string[] = []
+    let otherWinners: string[] = []
 
     eventData.awards?.forEach((award) => {
-        if (award[1] == 0 || award[1] == 69) {
+        if (params.slug === "cmptx" && award[1] == 0) {
+            hofWinners.push(award[0])
+        }
+        else if (award[1] == 0 || award[1] == 69) {
             impactWinners.push(award[0])
         } else if (award[1] == 9) {
             eiWinners.push(award[0])
+        } else {
+            if ([11, 22, 82, 10, 15, 13].includes(award[1]))
+            otherWinners.push(award[0])
         }
     })
 
@@ -127,18 +141,22 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
             <section className="w-full text-left mt-12 pt-10">
                 <h2>{eventData.name.includes("Division") ? "Engineering Inspiration" : "Impact"} Projections</h2>
-                <ProjectionsTable data={eventData.projections as Projection[]} impactWinners={impactWinners} eiWinners={eiWinners} />
+                <ProjectionsTable data={eventData.projections as Projection[]} impactWinners={impactWinners} eiWinners={eiWinners} awardWinners={otherWinners} hofWinners={hofWinners} />
                 <ul className="mt-6">
+                    {hofWinners.length > 0 ? <li className="flex my-1"><div className={`h-6 w-6 ${HOF_GRADIENT}`}></div> <p className="ml-2"> indicates team won CCA at event</p></li> : null}
                     <li className="flex my-1"><div className="h-6 w-6 bg-gradient-to-br from-yellow-300 via-yellow-400 to-yellow-500"></div> <p className="ml-2"> indicates team won Impact/Chairman{"'"}s at event</p></li>
                     <li className="flex my-1"><div className="h-6 w-6 bg-gradient-to-br from-gray-100 to-gray-300"></div> <p className="ml-2"> indicates team won Engineering Inspiration at event</p></li>
+                    <li className="flex my-1"><div className={`h-6 w-6 ${OTHER_GRADIENT}`}></div> <p className="ml-2"> indicates team won other IMPOWER-counting award</p></li>
                 </ul>
             </section>
             <section className="w-full text-left mt-12">
                 {eventData.ei_projections !== undefined ? <><h2>Engineering Inspiration Projections</h2>
-                    <ProjectionsTable data={eventData.ei_projections as Projection[]} impactWinners={impactWinners} eiWinners={eiWinners} />
+                    <ProjectionsTable data={eventData.ei_projections as Projection[]} impactWinners={impactWinners} eiWinners={eiWinners} awardWinners={otherWinners} hofWinners={hofWinners} />
                     <ul className="mt-6">
+                        {hofWinners.length > 0 ? <li className="flex my-1"><div className={`h-6 w-6 ${HOF_GRADIENT}`}></div> <p className="ml-2"> indicates team won CCA at event</p></li> : null}
                         <li className="flex my-1"><div className="h-6 w-6 bg-gradient-to-br from-yellow-300 via-yellow-400 to-yellow-500"></div> <p className="ml-2"> indicates team won Impact/Chairman{"'"}s at event</p></li>
                         <li className="flex my-1"><div className="h-6 w-6 bg-gradient-to-br from-gray-100 to-gray-300"></div> <p className="ml-2"> indicates team won Engineering Inspiration at event</p></li>
+                        <li className="flex my-1"><div className={`h-6 w-6 ${OTHER_GRADIENT}`}></div> <p className="ml-2"> indicates team won other IMPOWER-counting award</p></li>
                     </ul></> : null}
             </section>
         </div>
